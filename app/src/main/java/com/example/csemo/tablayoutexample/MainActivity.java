ba14fb0579a1c59dbbd171391e18ce95023a02f7
Md. Mushfique Hasan Monim <cse.monim@gmail.com>
2018-07-14 10:31:14 +0600

    create viewpager adapter and implement tablayout

--- app/src/main/java/com/example/csemo/tablayoutexample/MainActivity.java
+++ app/src/main/java/com/example/csemo/tablayoutexample/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.csemo.tablayoutexample;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.example.csemo.tablayoutexample.adapter.ViewPagerAdapter;
+import com.example.csemo.tablayoutexample.fragment.HomeFragment;
+import com.example.csemo.tablayoutexample.fragment.OfficeFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new HomeFragment(),"Home");
+        adapter.addFragment(new OfficeFragment(),"Office");
+        viewPager.setAdapter(adapter);
     }
 }
